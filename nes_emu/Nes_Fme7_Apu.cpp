4a97b87b0c88d654d220f604e99fcef66c5466a6
Christian Fetzer <fetzer.ch@gmail.com>
2015-12-28 16:31:12 +0100

    cleanups: Fix compilation without NDEBUG (dprintf)
    Commit a63b5a2202a32e13f451d7ded0b0191fe1f00894 removed the dprintf
    define and its usage in most of the places. Two places were left that
    prevent the core from being compiled with not defined NDEBUG.

--- nes_emu/Nes_Fme7_Apu.cpp
+++ nes_emu/Nes_Fme7_Apu.cpp
@@ -50,13 +50,6 @@ void Nes_Fme7_Apu::run_until( blip_time_t end_time )
 		if ( !oscs [index].output )
 			continue;
 		
-		// check for unsupported mode
-		#ifndef NDEBUG
-			if ( (mode & 011) <= 001 && vol_mode & 0x1f )
-				dprintf( "FME7 used unimplemented sound mode: %02X, vol_mode: %02X\n",
-						mode, vol_mode & 0x1f );
-		#endif
-		
 		if ( (mode & 001) | (vol_mode & 0x10) )
 			volume = 0; // noise and envelope aren't supported
 		
