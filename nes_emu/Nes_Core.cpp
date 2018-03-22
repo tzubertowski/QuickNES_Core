497684e3d0ef3a4b6ff45a9b83b5be58c627d86a
retrowertz <retro-wertz@users.noreply.github.com>
2018-03-22 17:29:07 +0800

    Use RETRO_MEMORY_SAVE_RAM only when battery is enabled

--- nes_emu/Nes_Core.cpp
+++ nes_emu/Nes_Core.cpp
@@ -63,6 +63,7 @@ void Nes_Core::close()
 	disable_rendering();
 }
 
+extern bool has_battery_ram;
 const char * Nes_Core::open( Nes_Cart const* new_cart )
 {
 	close();
@@ -72,12 +73,14 @@ const char * Nes_Core::open( Nes_Cart const* new_cart )
 	mapper = Nes_Mapper::create( new_cart, this );
 	if ( !mapper ) 
 		return unsupported_mapper;
-	
+
 	RETURN_ERR( ppu.open_chr( new_cart->chr(), new_cart->chr_size() ) );
 	
 	cart = new_cart;
+	has_battery_ram = cart->has_battery_ram();
 	memset( impl->unmapped_page, unmapped_fill, sizeof impl->unmapped_page );
 	reset( true, true );
+
 	return 0;
 }
 
