3d2159d28a35bb4d350c8616b89b1cbf00bf053c
retrowertz <retro-wertz@users.noreply.github.com>
2018-03-25 00:11:26 +0800

    VRC-3: Update IRQ and set correct mirroring

--- nes_emu/Mapper_Vrc3.cpp
+++ nes_emu/Mapper_Vrc3.cpp
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  * This mapper was added by retrowertz for Libretro port of QuickNES.
  * 3/24/18
  *
@@ -25,15 +25,13 @@
 
 struct vrc3_state_t
 {
-	uint8_t  prg_bank;
-	uint8_t  irq_control;
+	bool     irq_enable;
 	uint8_t  irq_pending;
-	uint8_t  unused;
-	uint16_t irq_latch;
+	uint16_t irq_counter;
 	uint16_t next_time;
 };
 
-BOOST_STATIC_ASSERT( sizeof ( vrc3_state_t ) == 8 );
+BOOST_STATIC_ASSERT( sizeof ( vrc3_state_t ) == 6 );
 
 class Mapper_VRC3 : public Nes_Mapper, vrc3_state_t {
 public:
@@ -50,19 +48,23 @@ public:
 	void apply_mapping()
 	{
 		enable_sram();
-		set_prg_bank( 0x8000, bank_16k, prg_bank );
+		mirror_vert();
 	}
 
 	virtual void run_until( nes_time_t end_time )
 	{
-		if ( irq_control & 2)
+		if ( irq_enable )
 		{
-			uint32_t tmp = irq_latch + ( end_time - next_time );
+			long counter = irq_counter + ( end_time - next_time );
 
-			if ( tmp > 0xffff )
+			if ( counter > 0xFFFF )
+			{
 				irq_pending = true;
+				irq_enable = false;
+				counter = 0xFFFF;
+			}
 
-			irq_latch = tmp;
+			irq_counter = counter;
 		}
 
 		next_time = end_time;
@@ -72,6 +74,7 @@ public:
 	{
 		if ( end_time > next_time )
 			run_until( end_time );
+
 		next_time -= end_time;
 	}
 
@@ -80,55 +83,34 @@ public:
 		if ( irq_pending )
 			return present;
 
-		if ( !(irq_control & 2 ) )
+		if ( !irq_enable )
 			return no_irq;
 
-		return 0x10000 - irq_latch + next_time;
+		return 0x10000 - irq_counter + next_time;
+	}
+
+	void write_irq_counter( int shift, int data )
+	{
+		irq_counter &= ~( 0x0F << shift );
+		irq_counter |= data << shift;
 	}
 
 	void write( nes_time_t time, nes_addr_t addr, int data )
 	{
-		if ( ( addr & 0xF000 ) == 0xF000 )
+		data &= 0xF;
+		irq_pending = false;
+
+		switch ( addr >> 12 )
 		{
-			prg_bank = data;
-			set_prg_bank( 0x8000, bank_16k, data );
+			case 0x8: write_irq_counter(  0, data ); break;
+			case 0x9: write_irq_counter(  4, data ); break;
+			case 0xA: write_irq_counter(  8, data ); break;
+			case 0xB: write_irq_counter( 12, data ); break;
+			case 0xC: irq_enable = data & 2; break;
+			case 0xF: set_prg_bank( 0x8000, bank_16k, data ); break;
 		}
 
-		else
-		{
-			run_until( time );
-
-			switch ( addr & 0xF000 )
-			{
-				case 0x8000:
-					irq_latch &= 0xFFF0;
-					irq_latch |= ( data & 0xF );
-					break;
-				case 0x9000:
-					irq_latch &= 0xFF0F;
-					irq_latch |= ( data & 0xF ) << 4;
-					break;
-				case 0xa000:
-					irq_latch &= 0xF0FF;
-					irq_latch |= ( data & 0xF ) << 8;
-					break;
-				case 0xb000:
-					irq_latch &= 0x0FFF;
-					irq_latch |= ( data & 0xF ) << 12;
-					break;
-				case 0xc000:
-					irq_pending = false;
-					irq_control = data;
-					irq_changed();
-					break;
-				case 0xd000:
-					irq_pending = false;
-					irq_control &= ~2;
-					irq_control |= ( irq_control << 1 ) & 2;
-					irq_changed();
-					break;
-			}
-		}
+		irq_changed();
 	}
 };
 
